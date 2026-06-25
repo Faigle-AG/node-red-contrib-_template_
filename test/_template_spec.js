ac713ba36aa105392b7b5dc236ef211cf3dea190
Peter Michailis <peter.michailis@faigle.ch>
2026-06-25 12:00:39 +0200

    added linter

--- test/_template_spec.js
+++ test/_template_spec.js
@@ -11,7 +11,7 @@ describe('example-template Node', function () {
 
   it('should load correctly with the given name', function (done) {
     const flow = [{ id: "n1", type: "_template_", name: "my template" }];
-    
+
     helper.load(templateNode, flow, function () {
       const n1 = helper.getNode("n1");
       try {
@@ -28,11 +28,11 @@ describe('example-template Node', function () {
       { id: "n1", type: "_template_", wires:[["n2"]] },
       { id: "n2", type: "helper" }
     ];
-    
+
     helper.load(templateNode, flow, function () {
       const n1 = helper.getNode("n1");
       const n2 = helper.getNode("n2");
-      
+
       n2.on("input", function (msg) {
         try {
           msg.should.have.property('payload', 'hello world');
@@ -41,7 +41,7 @@ describe('example-template Node', function () {
           done(err);
         }
       });
-      
+
       n1.receive({ payload: "hello world" });
     });
   });
