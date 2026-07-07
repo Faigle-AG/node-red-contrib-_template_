bc413c998447195b80722e42b1b98d51ad569fd3
Peter Michailis <peter.michailis@faigle.ch>
2026-07-07 02:11:00 +0200

    refactor: reverting to prior implementation

--- src/_template_.js
+++ src/_template_.js
@@ -3,36 +3,28 @@ module.exports = function (RED) {
 
     function ExampleTemplateNode(config) {
         RED.nodes.createNode(this, config);
-
         this.name = config.name;
         this.enableLogging = config.enableLogging;
 
-        const node = this;
+        var node = this;
 
         extendStatus(node);
         extendProperties(node);
 
         node.on('input', function (msg, send, done) {
-            send =
-                send ||
-                function () {
-                    node.send.apply(node, arguments);
-                };
-
-            try {
-                if (node.enableLogging) {
-                    node.log('Received payload: ' + JSON.stringify(msg.payload));
-                }
-
-                node.status.processing('processing message');
-
-                send(msg);
-
-                if (done) done();
-            } catch (err) {
-                if (done) done(err);
-                else node.error(err, msg);
+            if (node.enableLogging) {
+                node.log('Received payload: ' + JSON.stringify(msg.payload));
             }
+
+            node.status.processing('processing message');
+
+            send(msg);
+
+            if (done) done();
+
+            node.status.succeeded('finished processing', {
+                next: () => node.status.waiting('waiting for input'),
+            });
         });
     }
 
